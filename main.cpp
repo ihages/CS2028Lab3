fbf7e0365591655f50f8e3c4768ec696cad179a4
ihages <hagemaij@mail.uc.edu>
2025-02-09 00:06:03 -0500

    main.cpp: implemented loop system
    Can we get this code condensed any way by maybe making a void funtion that implements y? The code currently works, still unsure about what the print funtion's deal is (can we bypass that with the print funtion?).

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include "Design.h"
 
 int main() {
+	
+
+	/*****
+	* std::cout << "When running the function with no parameters," << std::endl;
+	* Design d1;
+	* d1.print();
+	* 
+	* changes after implementation to design.h: 
+	* put constructors in main rather than design.h
+	*****/
 
 	int littleInput;
 	int lotInput;
@@ -19,62 +29,123 @@ int main() {
 	Design x(littleInput, lotInput, heapInput);
 	//x.print();
 
-	std::cout << "\nEnter the number of littles, lots, and heaps for y" << std::endl;
-	std::cout << "Littles: ";
-	std::cin >> littleInput;
-	std::cout << "Lots: ";
-	std::cin >> lotInput;
-	std::cout << "Heaps: ";
-	std::cin >> heapInput;
+	char contPrompt{ 'y' };
 
-	//std::cout << "\ny:" << std::endl;
-	Design y(littleInput, lotInput, heapInput);
-	//y.print();
-	
+	while (contPrompt == 'y') {
 		int opPrompt{ 0 };
 
-	std::cout << "\nWhat opperation do you want to do?" << std::endl;
-	std::cout << "1. add\n2. subtract\n3. multiply\n4. divide\n5. compare\n6. print\n" << std::endl;
+		std::cout << "\nWhat opperation do you want to do?" << std::endl;
+		std::cout << "1. add\n2. subtract\n3. multiply\n4. divide\n5. compare\n6. print\n" << std::endl;
 
-	std::cin >> opPrompt;
-	std::cout << std::endl;
+		std::cin >> opPrompt;
+		std::cout << std::endl;
 
-	if (opPrompt == 1) {
-		std::cout << "Adding x and y" << std::endl;
-		x + y;
-		x.print();
-	}
-	else if (opPrompt == 2) {
-		std::cout << "Subtacting x by y" << std::endl;
-		x - y;
-		x.print();
-	}
-	else if (opPrompt == 3) {
-		std::cout << "Multiplying x and y" << std::endl;
-		x* y;
-		x.print();
-	}
-	else if (opPrompt == 4) {
-		std::cout << "Dividing x by y" << std::endl;
-		x / y;
-		x.print();
-	}
-	else if (opPrompt == 5) {
-		std::cout << "Comparing x and y" << std::endl;
-		if (x == y) {
-			std::cout << "The two positions are the same" << std::endl;
+		if (opPrompt == 1) {
+			std::cout << "\nEnter the number of littles, lots, and heaps for y" << std::endl;
+			std::cout << "Littles: ";
+			std::cin >> littleInput;
+			std::cout << "Lots: ";
+			std::cin >> lotInput;
+			std::cout << "Heaps: ";
+			std::cin >> heapInput;
+
+			//std::cout << "\ny:" << std::endl;
+			Design y(littleInput, lotInput, heapInput);
+			//y.print();
+
+			std::cout << "\nAdding y to x" << std::endl;
+			x + y;
+			x.print();
+		}
+		else if (opPrompt == 2) {
+			std::cout << "\nEnter the number of littles, lots, and heaps for y" << std::endl;
+			std::cout << "Littles: ";
+			std::cin >> littleInput;
+			std::cout << "Lots: ";
+			std::cin >> lotInput;
+			std::cout << "Heaps: ";
+			std::cin >> heapInput;
+
+			//std::cout << "\ny:" << std::endl;
+			Design y(littleInput, lotInput, heapInput);
+			//y.print();
+
+			std::cout << "\nSubtracting x by y" << std::endl;
+			x - y;
+			x.print();
+		}
+		else if (opPrompt == 3) {
+			std::cout << "\nEnter the number of littles, lots, and heaps for y" << std::endl;
+			std::cout << "Littles: ";
+			std::cin >> littleInput;
+			std::cout << "Lots: ";
+			std::cin >> lotInput;
+			std::cout << "Heaps: ";
+			std::cin >> heapInput;
+
+			//std::cout << "\ny:" << std::endl;
+			Design y(littleInput, lotInput, heapInput);
+			//y.print();
+
+			std::cout << "\nMultiplying x by y" << std::endl;
+			x* y;
+			x.print();
+		}
+		else if (opPrompt == 4) {
+			std::cout << "\nEnter the number of littles, lots, and heaps for y" << std::endl;
+			std::cout << "Littles: ";
+			std::cin >> littleInput;
+			std::cout << "Lots: ";
+			std::cin >> lotInput;
+			std::cout << "Heaps: ";
+			std::cin >> heapInput;
+
+			//std::cout << "\ny:" << std::endl;
+			Design y(littleInput, lotInput, heapInput);
+			//y.print();
+
+			std::cout << "\nDividing x by y" << std::endl;
+			x / y;
+			x.print();
+		}
+		else if (opPrompt == 5) {
+			std::cout << "\nEnter the number of littles, lots, and heaps for y" << std::endl;
+			std::cout << "Littles: ";
+			std::cin >> littleInput;
+			std::cout << "Lots: ";
+			std::cin >> lotInput;
+			std::cout << "Heaps: ";
+			std::cin >> heapInput;
+
+			//std::cout << "\ny:" << std::endl;
+			Design y(littleInput, lotInput, heapInput);
+			//y.print();
+
+			std::cout << "\nComparing x to y" << std::endl;
+			if (x == y) {
+				std::cout << "The two positions are the same" << std::endl;
+			}
+			else {
+				std::cout << "The two positions are not the same" << std::endl;
+			}
+		}
+		else if (opPrompt == 6) {
+			std::cout << "\nCoverting x to a string" << std::endl;
+			x.print();
+			//std::cout << x << std::endl;
 		}
 		else {
-			std::cout << "The two positions are not the same" << std::endl;
+			std::cout << "Invalid Opperation";
 		}
-	}
-	else if (opPrompt == 6) {
-		//std::cout << "Coverting x and y to a string" << std::endl;
-		//std::cout << x << std::endl;
-	}
-	else {
-		std::cout << "Invalid Opperation";
-	}
+
+		std::cout << "\nWould you like to continue? y/n" << std::endl;
+		std::cin >> contPrompt;
+		while (contPrompt != 'y' && contPrompt != 'n'){
+			std::cout << "Invalid Response. Try again." << std::endl;
+		std::cin >> contPrompt;
+		}	
+	} // while closer
+
 
     return 0;
 } // main closer
